e6f74631a1316cd64fa9a36bedb5de0e73e0e469
LiliaHxH <l22171282@culiacan.tecnm.mx>
2025-03-11 22:57:19 +0000

    Creacion de ecenarios

--- sketch.js
+++ sketch.js
@@ -1,14 +1,51 @@
+ let imagenFondo
+ let imagenInicio
+ let personaje
+ let x=0
+ let posY=100
+ let dY=3
+ let estado=0//0:menu 1:
+
 function preload() {
   // put preload code here
+  imagenFondo=loadImage("./images/fondojuego00.png")
+  personaje=loadImage("./images/miku00.gif")
 }
 
 function setup() {
   // put setup code here
   createCanvas(1000,512)
+  noCursor()
 }
 
 function draw() {
   // put drawing code here
-  background(255)
-  rect(100,100,200,200)
+  if(estado===1){
+      background(255)
+      image(imagenFondo,x,0)
+      image(imagenFondo,x+imagenFondo.width,0)
+      x=x-5
+      dY=dY+1
+      posY=posY+dY
+      if(x<=-imagenFondo.width){
+        x=0
+      }
+      image(personaje,100,posY,60,60)
+  }else if(estado===0){
+    image(imagenFondo,0,0)
+    textSize(50)
+    fill(255)
+    text("HAGA CLICK PARA COMENZAR",100,100)
+  }
+}
+function mousePressed(){
+  //posY=100
+  if(estado===0){
+    estado=1
+    posY=100
+    x=0
+    dY=3
+    noCursor()
+  }
+  dY=-15
 }
