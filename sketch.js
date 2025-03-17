ae1a2a941c9c76f49b6a9e68a9aa3e8d5d9b3991
LiliaHxH <l22171282@culiacan.tecnm.mx>
2025-03-17 18:20:10 +0000

    JUEGO MIKUBIRD FINALIZADOOOOO

--- sketch.js
+++ sketch.js
@@ -1,88 +1,113 @@
-let imagenFondo;
-let imagenInicio;
-let personaje;
-let x = 0;
-let posY = 100;
-let dY = 3;
-let estado = 0; // 0: menu ; 1: jugando
-let wallX = [];
-let wallY = [];
-let pared;
-puntaje = 0;
-puntajeMax = 10;
-recordAnterior = 0;
+let imagenFondo
+let imagenInicio
+let personaje
+let pared
+let x = 0
+let posY = 100
+let dY = 3
+let estado = 0 //0: menú; 1: jugando; 2: gameOver
+let wallX = []
+let wallY = []
+let puntaje = 0
+let puntajeMax = 0
+let recordAnterior = 0
+let musicaRecord
+let musicaJuego
 
 function preload() {
   // put preload code here
-  imagenFondo = loadImage("./images/fondojuego00.png");
-  imagenInicio = loadImage("./images/images.jpg");
-  personaje = loadImage("./images/miku00.gif");
-  pared = loadImage("./images/pared.png");
+  imagenFondo = loadImage("./images/fondojuego00.png")
+  imagenInicio = loadImage("./images/R.jpg")
+  personaje = loadImage("./images/miku00.gif")
+  pared = loadImage("./images/pared.png")
+  musicaRecord = loadSound("./sounds/aplauso.wav")
+  musicaJuego = loadSound("./sounds/musicafondo.mp3")
 }
 
 function setup() {
   // put setup code here
   createCanvas(1000,512)
-  noCursor();
+  noCursor()
 }
 
 function draw() {
-  // put drawing code here
-  if (estado == 1){
-    imageMode(CORNER);``
+  // put d(rawing code here
+  if (estado === 1) {
+    imageMode(CORNER)
     background(255)
-    image(imagenFondo,x,0);
-    image(imagenFondo, x + imagenFondo.width,0);
-    x = x-5;
-    dY = dY + 1;
-    posY = posY + dY;
-    if(x <= -imagenFondo.width){
-      x = 0;
-      
+    image(imagenFondo,x,0)
+    image(imagenFondo, x + imagenFondo.width, 0)
+    x = x - 5
+    dY = dY + 1
+    posY = posY + dY
+    if (x <= -imagenFondo.width) {
+      x = 0
     }
-    // obstaculos
-    for (let i=0; i<wallX.length; i++){
-      imageMode(CENTER);
-      image(pared,wallX[i],wallY[i]- 500)
-      image(pared,wallX[i],wallY[i] + 500)
-      if(wallX[i] < 0 ){
-        wallX[i] = width;
-        wallY[i] = random(200,300);
+    //Obstáculos
+    for (let i=0; i<wallX.length; i++) {
+      imageMode(CENTER)
+      image(pared,wallX[i],wallY[i]-500)
+      image(pared,wallX[i],wallY[i]+500)
+
+      if (wallX[i] < 0) {
+        wallX[i] = width
+        wallY[i] = random(200,300)
       }
-          //puntaje
-         if(wallX[i] == 100){
-          puntaje+=1;
-          puntajeMax = max(puntajeMax,puntaje);
-          }
-      wallX[i] = wallX[i] -5; // para que se muevan los obstaculos
+
+      //Puntaje
+      if (wallX[i] === 100) {
+        puntaje = puntaje + 1
+        puntajeMax = max(puntajeMax,puntaje)
+      }
+
+      wallX[i] = wallX[i] - 5  //Para que se muevan los obstáculos
+      if (posY < -60 || posY > height+60 
+               || (abs(wallX[i]-100) < 60 
+               && abs(wallY[i]-posY)>100)) {
+        musicaJuego.stop()
+        estado = 0
+      }
+
     }
 
-    // personajes
-    image(personaje,100,posY,60,60);
-    text("Puntaje: " + puntaje,width/2 - 60 ,30);
-}else if (estado === 0 ){
-    cursor();
-    image(imagenInicio,0,0)
-    textSize(50);
-    fill(255);
-    text("Puntaje maximo: " + puntajeMax,600,100);
-    text("Haga click para comenzar",600,200);
+    //Personaje
+    image(personaje, 100, posY,60,60)
+    text("Puntaje: " + puntaje, width/2-60,30)
+  } else if (estado === 0) {
+    background(0)
+    imageMode(CORNER)
+    cursor()
+    image(imagenInicio, 0, 0,450,600)
+    textSize(24)
+    fill(255)
+    text("Puntaje Máximo: " + puntajeMax, 600, 100)
+    text("Haga clic para comenzar", 600, 200)
+    if (puntajeMax > recordAnterior) {
+      if (!musicaRecord.isPlaying()) {
+        musicaRecord.play()
+      }
+    }
   }
 }
-function mousePressed(){
-  //posY = 100;
-  if (estado === 0 ){
+
+function mousePressed() {
+  //posY = 100
+  if (estado === 0) {
     estado = 1
-    posY = 100;
-    dY = 3;
-    x = 0;
-    wallX = [500,800,1100];
-    wallY[0] = random(200,100)
-    wallY[1] = random(200,100)
-    wallY[2] = random(200,100)
-    puntaje = 0;
+    posY = 100
+    x = 0
+    dY = 3
+    wallX = [500, 800,1100]
+    wallY[0] = random(200,300)
+    wallY[1] = random(200,300)
+    wallY[2] = random(200,300)
+    puntaje = 0
     recordAnterior = puntajeMax
-    noCursor();
+    noCursor()
+    if (musicaRecord.isPlaying()) {
+      musicaRecord.stop()
+    }
+    musicaJuego.loop()
   }
-  dY = -15;
+  dY = -15
 }
